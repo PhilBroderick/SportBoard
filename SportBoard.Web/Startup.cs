32635b41f823f4c3c5bf889ca531fc4102a82adb
philbroderick12@gmail.com <philbroderick12@gmail.com>
2018-11-21 20:36:03 +0000

    Add project files.

---
+++ SportBoard.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportBoard.Web.Startup))]
+namespace SportBoard.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
