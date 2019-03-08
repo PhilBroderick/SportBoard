4fe2613915afcc01d267b5eb269a2d727729f95d
Phil <philbroderick12@gmail.com>
2019-03-08 14:01:40 +0000

    Displays new notifications on the notification screen - need to make them 'readable' via a click of the notification and display a figure of unread notifications on the nav bar

--- SportBoard.Web/Controllers/UserNotificationController.cs
+++ SportBoard.Web/Controllers/UserNotificationController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using SportBoard.Data.DAL;
+using SportBoard.Data.DAL.Respositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +12,24 @@ namespace SportBoard.Web.Controllers
     [RoutePrefix("notifications")]
     public class UserNotificationController : Controller
     {
+        private SportboardDbContext _context;
+        private UnitOfWork _unitOfWork;
+        private UserNotificationRepository _userNotificationRepository;
+
+        public UserNotificationController()
+        {
+            _context = new SportboardDbContext();
+            _unitOfWork = new UnitOfWork(_context);
+            _userNotificationRepository = new UserNotificationRepository(_context);
+        }
+
         [Route("")]
         public ActionResult Index()
         {
-            return View();
+            var currentUserId = User.Identity.GetUserId();
+            var notifications = _userNotificationRepository.GetUnreadNotificationsByUserId(currentUserId).ToList();
+
+            return View(notifications);
         }
     }
 }
