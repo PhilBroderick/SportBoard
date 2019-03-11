63f74c71fa704fd141448e41dcd649a691a4fa4b
Phil <philbroderick12@gmail.com>
2019-03-11 18:44:17 +0000

    Add Forloop Html Helpers nuget package - trying to get unread notifications on nav bar

---
+++ SportBoard.Web/Scripts/Custom/UserNotifications.js
@@ -0,0 +1,16 @@
+﻿var unreadNotificationCount = 0;
+
+function getUnreadNotifications() {
+    $.ajax({
+        type: "GET",
+        url: "/api/notifications?userId=" + '@User.Identity.GetUserId()',
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            unreadNotificationCount = response.notificationCount;
+        },
+        error: function () {
+            alert("error");
+        }
+    })
+}
